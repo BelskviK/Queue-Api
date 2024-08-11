3fe128d6324e027f5de360d26c104161b49bf7d5
Vano Gogebashvili <122786020+BelskviK@users.noreply.github.com>
2024-08-11 16:12:11 +0400

    Update database.js

--- src/shared/database.js
+++ src/shared/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function connectToMongoDB() {
   try {
     mongoose.connect(
-      "mongodb+srv://root:root@jobebi.pijgp8i.mongodb.net/?retryWrites=true&w=majority&appName=Jobebi"
+      ""
     );
     console.log("Conected To MongoDB");
   } catch (error) {
